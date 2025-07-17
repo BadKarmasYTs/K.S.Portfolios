b8067064c4e8823aaae3275bd4e3694de839627b
BadKarmasYTs <lukezuskas@gmail.com>
2025-07-16 20:50:34 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+window.games = [
+  { name: "Dumpster Dive", image: "assets/images/dumpster.png" },
+  { name: "Worker Tycoon", image: "assets/images/worker.png" },
+  { name: "Island Builder", image: "assets/images/island.png" },
+  { name: "Pet Adventure", image: "assets/images/pet.png" },
+  { name: "Alien Farm", image: "assets/images/alien.png" },
+];
+
+window.groups = [
+  { name: "KS Studios", image: "assets/images/ks.png" },
+  { name: "Dev Blocks", image: "assets/images/devblocks.png" },
+  { name: "Noob Inc", image: "assets/images/noob.png" },
+  { name: "Code Core", image: "assets/images/codecore.png" },
+  { name: "World Devs", image: "assets/images/worlddevs.png" },
+  { name: "PlayLabs", image: "assets/images/playlabs.png" },
+  { name: "Sim Builders", image: "assets/images/simbuilders.png" },
+  { name: "Pixel Forge", image: "assets/images/pixelforge.png" },
+  { name: "GameSpark", image: "assets/images/gamespark.png" },
+  { name: "Hyper Devs", image: "assets/images/hyperdevs.png" },
+];
+
+window.people = [
+  { name: "Hexel", role: "Builder", image: "assets/images/hexel.png" },
+  { name: "Zeno", role: "Animator", image: "assets/images/zeno.png" },
+  { name: "Nova", role: "UI Designer", image: "assets/images/nova.png" },
+  { name: "Byte", role: "Scripter", image: "assets/images/byte.png" },
+  { name: "Cloudy", role: "FX Artist", image: "assets/images/cloudy.png" },
+];
