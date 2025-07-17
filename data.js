82436c35d8ebdae3b2c4d2c17e0e2041f2ded7c1
BadKarmasYTs <lukezuskas@gmail.com>
2025-07-17 14:46:37 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,26 @@
 window.games = [
-  { name: "Dumpstered [⭐]",   image: "https://i.postimg.cc/rwQSWrzj/image.png",  desc: "This was my most favorite game ideas Ive ever came up with and I belive me and my team executed it really well." },
-  { name: "Worker Tycoon", image: "assets/images/worker.png", desc: "Place NPCs and automate your income." },
-  { name: "Island Builder", image: "assets/images/island.png", desc: "Design and expand your own tropical land." },
-  { name: "Pet Adventure", image: "assets/images/pet.png", desc: "Collect and level pets in different zones." },
-  { name: "Alien Farm", image: "assets/images/alien.png", desc: "Grow alien plants with placed workers." }
+  { name: "Dumpstered [⭐]",   image: "https://i.postimg.cc/rwQSWrzj/image.png",  desc: "This was my most favorite game idea Ive ever came up with and I belive me and my team executed it really well." },
+  { name: "Spray It [🎨]", image: "https://i.postimg.cc/Qx1RDY7W/New-Project-36.png", desc: "First ever game I made that got more than 100 visits!" },
+  { name: "Meteor Mayhem", image: "https://i.postimg.cc/g0JSxgxs/imgi-74-no-Filter.webp", desc: "One of the first games that I put my heart and sole into." },
+  { name: "🎮 NEED MORE VIDEO GAMES", image: "https://i.postimg.cc/nrcCx9RX/imgi-73-no-Filter.webp", desc: "Made a few tasks and the opening cutscene to this game." },
+  { name: "Death Spinner 💀", image: "https://i.postimg.cc/rF7FnHk0/imgi-71-no-Filter.webp", desc: "Invested in this game pushing it to where it is now." },
+    { name: "Rollbux", image: "https://i.postimg.cc/tCFhpwzP/New-Project-29.png", desc: "Worked on this game for a bit, before it was taken down due to a developer being accused." }
 ];
 
 window.groups = [
-  { name: "KS Studios", image: "assets/images/ks.png", desc: "Main dev group and original inspiration." },
-  { name: "Dev Blocks", image: "assets/images/devblocks.png", desc: "Focused on block-based simulation games." },
-  { name: "Noob Inc", image: "assets/images/noob.png", desc: "Casual tycoon creators." },
-  { name: "Code Core", image: "assets/images/codecore.png", desc: "Script-focused development team." },
-  { name: "World Devs", image: "assets/images/worlddevs.png", desc: "Specializes in immersive worlds." },
-  { name: "PlayLabs", image: "assets/images/playlabs.png", desc: "Prototyping group for experiments." },
-  { name: "Sim Builders", image: "assets/images/simbuilders.png", desc: "Full-simulation based games." },
-  { name: "Pixel Forge", image: "assets/images/pixelforge.png", desc: "Stylized game creators." },
-  { name: "GameSpark", image: "assets/images/gamespark.png", desc: "Dynamic Roblox games." },
-  { name: "Hyper Devs", image: "assets/images/hyperdevs.png", desc: "Fast-paced dev group." }
+  { name: "KS Studios", image: "https://i.postimg.cc/Sx0k7z6c/Frame-21.png", desc: "Owner and Founder Of K.S Studios!" },
+  { name: "Snazks", image: "https://i.postimg.cc/Bt9kDHr6/noFilter.webp", desc: "Second Owner Of Snazks and Lead Developer!" },
+  { name: "NEXUS", image: "https://i.postimg.cc/j2PKGxKD/noFilter.webp", desc: "Planned to be used down the road." },
+  { name: "Unreversed Studios", image: "https://i.postimg.cc/J0mdzMZD/noFilter.webp", desc: "I'm an Underpaid Employee for this group founded by my greatest developer friend." },
+  { name: "Studio Cyber", image: "https://i.postimg.cc/pdHj7rHF/noFilter.webp", desc: "Lead Developer currently producing some of roblox's next top games." },
+  { name: "Pumpkin Studios!?", image: "https://i.postimg.cc/2ysyZ9jZ/noFilter.webp", desc: "Co owner of this group" },
 ];
 
 window.people = [
-  { name: "Hexel", role: "Builder", image: "assets/images/hexel.png", desc: "Map and environment builder." },
-  { name: "Zeno", role: "Animator", image: "assets/images/zeno.png", desc: "Creates smooth cutscenes and emotes." },
-  { name: "Nova", role: "UI Designer", image: "assets/images/nova.png", desc: "Stylish, mobile-friendly interfaces." },
-  { name: "Byte", role: "Scripter", image: "assets/images/byte.png", desc: "Handles backend and systems." },
-  { name: "Cloudy", role: "FX Artist", image: "assets/images/cloudy.png", desc: "Particles, weather, and ambience." }
+  { name: "Blaze", role: "Scripter/Investor", image: "https://i.postimg.cc/Y2RHZq5m/imgi-72-e5829e0b647546143481375f3b803e04.webp", desc: "Blaze is the owner of Risky Haul + A good friend of mine." },
+  { name: "GlassTDM", role: "Scripter/Bff😉", image: "https://i.postimg.cc/PJT8xryw/imgi-90-f12ec353a13e527a0a526c5db4a91134.webp", desc: "Words cant describe how good his builds are, He built and designed the whole Dumpstered Map!" },
+    { name: "Zarcules", role: "Builder", image: "https://i.postimg.cc/wBrV7JRs/imgi-46-5eb3ed4c15784de64b4cc6b291978b7f.webp", desc: "Created the whole map for my game Spray it, and now hes founding his own company." },
+  { name: "Pumpkin", role: "UI Designer", image: "https://i.postimg.cc/jjtcpt0L/imgi-88-7f5b210a61b0416856471a2c068559db.webp", desc: "Very Very Talented Ui designer who helped introduce me to some wonderful roblox devs!" },
+  { name: "Game", role: "Investor", image: "https://i.postimg.cc/1zmTppzC/imgi-49-2cb4fd1b74b965b92c7987e1785be9e6.webp", desc: "Game invested in my game Dumpstered helping it take off." },
+  { name: "Vwen", role: "Builder", image: "https://i.postimg.cc/wBrV7JRs/imgi-46-5eb3ed4c15784de64b4cc6b291978b7f.webp", desc: "Created the whole map for my game Spray it, and now hes founding his own company." }
 ];
