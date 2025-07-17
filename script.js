18e3d8c549b4b9ce51874b7e95bca8fb8d12036b
BadKarmasYTs <lukezuskas@gmail.com>
2025-07-16 20:50:46 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function createCard(data) {
+  const div = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = data.image;
+  div.appendChild(img);
+
+  const title = document.createElement("h3");
+  title.textContent = data.name;
+  div.appendChild(title);
+
+  if (data.role) {
+    const role = document.createElement("p");
+    role.textContent = data.role;
+    div.appendChild(role);
+  }
+
+  return div;
+}
+
+function loadCarousel(containerClass, data) {
+  const container = document.querySelector(containerClass);
+  data.forEach(item => {
+    container.appendChild(createCard(item));
+  });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadCarousel(".games-carousel", window.games);
+  loadCarousel(".groups-carousel", window.groups);
+  loadCarousel(".people-carousel", window.people);
+});
